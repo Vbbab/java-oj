af815b578fa4df71d7836a1771c5de982a3a21f6
khuang828 <khuang828@LHS-LAB608-39.local>
2022-05-10 10:21:23 -0700

    Implement `run()` in `Judger`; Update constants & gitignore temporary/testing dirs

---
+++ Judger.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*; // Time limits stuff
+
+/**
+ * Core class for Judger
+ * Utility class only 
+ */
+public class Judger {
+    private Judger() {}
+
+    public static String run(String file, String in) throws Exception {
+        Process p = Runtime.getRuntime().exec(Constants.BIN_DIR + file);
+        OutputStream stdin = p.getOutputStream();
+        InputStream stdout = p.getInputStream();
+        stdin.write(in.getBytes());
+
+        Callable<byte[]> readTask = new Callable<byte[]>() {
+            @Override
+            public byte[] call() throws Exception {
+                return stdout.readAllBytes();
+            }
+        };
+
+        ExecutorService e = Executors.newFixedThreadPool(2); // Create new thread
+
+        Future<byte[]> f = e.submit(readTask);
+
+        byte[] result = {0x00};
+
+        try {
+            result = f.get(Constants.MAX_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        } catch(TimeoutException tException) {
+            System.out.println("[TIMED OUT AFTER " + Constants.MAX_TIMEOUT_MILLIS / 1000 + " SECS]");
+        }
+        
+        // Cleanup
+        e.shutdownNow();
+        stdout.close();
+        stdin.close();
+        p.destroy();
+
+        return new String(result);
+    }
+}
