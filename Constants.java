d2bb97a6ade9429e47e5e0d2603a0778577d8631
khuang828 <khuang828@LHS-LAB608-39.local>
2022-05-10 09:21:05 -0700

    Add file w/ constants

---
+++ Constants.java
@@ -0,0 +1,13 @@
+/**
+  * Constants (e.g. paths) used throughout the code.
+ */
+public final class Constants {
+  private Constants() {}
+
+  /* PATHS */
+  public static final String TEMP_DIR = "tmp/";
+  public static final String SRC_DIR = TEMP_DIR + "src/";
+  public static final String BIN_DIR = TEMP_DIR + "bin/";
+  public static final String TC_DIR = "testcase/";
+
+}
